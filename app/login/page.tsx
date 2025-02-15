85bc28494498c20dfe2f53a82b7cc3da55fe7fe8
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-02-15 13:46:38 +0530

    text color fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -29,25 +29,27 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <form onSubmit={handleLogin} className="p-6 bg-black shadow rounded">
-        <h1 className="text-xl font-bold mb-4">Admin Login</h1>
+        <h1 className="text-xl font-bold mb-4 text-white">Admin Login</h1>
         {error && <p className="text-red-500 mb-2">{error}</p>}
         <div className="mb-4">
-          <label className="block text-sm font-medium">Email</label>
+          <label className="block text-sm text-white font-medium">Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             className="w-full px-3 text-black py-2 border rounded"
+            placeholder="Email"
             required
           />
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Password</label>
+          <label className="block text-sm text-white font-medium">Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full px-3 py-2 border text-black rounded"
+            placeholder="Password"
             required
           />
         </div>
